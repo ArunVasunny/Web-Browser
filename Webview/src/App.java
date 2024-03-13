2d6ff2b10bf1129ae8a69ec9e27c34ebd9a18b7a
ArunVasunny <arunvasunny153@gmail.com>
2024-03-13 18:11:17 +0530

    implemented scene to load

--- Webview/src/App.java
+++ Webview/src/App.java
@@ -1,5 +1,23 @@
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("webview.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Browser");
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+
     }
 }
