9b08eb93f1cb56fb2fb112e25b698d54ef78da76
ArunVasunny <arunvasunny153@gmail.com>
2024-03-14 19:47:04 +0530

    added app icon

--- Webview/src/App.java
+++ Webview/src/App.java
@@ -2,22 +2,27 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application{
+
+    Image img = new Image(getClass().getResourceAsStream("/Images/icon.png"));
+
     public static void main(String[] args) {
         launch(args);
     }
-
+    
     @Override
     public void start(Stage stage) throws Exception {
-
+        
         Parent root = FXMLLoader.load(getClass().getResource("webview.fxml"));
         Scene scene = new Scene(root);
-        stage.setTitle("Browser");
+        stage.setTitle("NetPulse Browser");
+        stage.getIcons().add(img);
         stage.setScene(scene);
         stage.show();
-        stage.setResizable(false);
+        stage.setResizable(false); 
 
     }
 }
